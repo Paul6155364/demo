eee3bab3f9619b8c23d9cb906bbde2982593d0cd
小力王 <6155364@qq.com>
2022-04-25 18:05:18 +0800

    静态代理

---
+++ src/main/java/com/example/demo/thread/StacticProxy.java
@@ -0,0 +1,39 @@
+package com.example.demo.thread;
+
+public class StacticProxy {
+    public static void main(String[] args) {
+        WeddingCompany weddingCompany = new WeddingCompany(new You());
+        weddingCompany.HappyMarry();
+    }
+}
+interface Marry{
+    void HappyMarry();
+}
+class You implements Marry{
+    @Override
+    public void HappyMarry() {
+        System.out.println("秦老师要结婚了");
+    }
+}
+
+class WeddingCompany implements Marry{
+
+    private Marry target;
+    public WeddingCompany(Marry target){
+        this.target = target;
+    }
+    @Override
+    public void HappyMarry() {
+        before();
+        this.target.HappyMarry();
+        after();
+    }
+
+    private void after() {
+        System.out.println("布置");
+    }
+
+    private void before() {
+        System.out.println("收钱");
+    }
+}

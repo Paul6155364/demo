9b0c08aa3759ea590955787a6e2fa0ee4db5e8e3
小力王 <6155364@qq.com>
2022-04-25 21:27:10 +0800

    yield

---
+++ src/main/java/com/example/demo/thread/TestJoin.java
@@ -0,0 +1,25 @@
+package com.example.demo.thread;
+
+public class TestJoin implements Runnable{
+    @Override
+    public void run() {
+        for(int i=0;i<100;i++){
+            System.out.println("vip"+i);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TestJoin testJoin = new TestJoin();
+        Thread thread = new Thread(testJoin);
+        thread.start();
+        for (int i=0;i<300;i++){
+            if(i==200){
+                thread.join();
+            }
+            System.out.println("main"+i);
+        }
+
+    }
+}
+
+

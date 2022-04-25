ab17a47c14a72b0ec0fcf978da910912e7e71b95
小力王 <6155364@qq.com>
2022-04-25 22:19:09 +0800

    yield

--- src/main/java/com/example/demo/thread/syn/UnsafeBank.java
+++ src/main/java/com/example/demo/thread/syn/UnsafeBank.java
@@ -1,4 +1,42 @@
 package com.example.demo.thread.syn;
 
 public class UnsafeBank {
+    public static void main(String[] args) {
+        Account account = new Account(100,"结婚基金");
+        Drawing you = new Drawing(account,50,"qq");
+        you.start();
+    }
+}
+class Account{
+    int money;
+    String name;
+
+    public Account(int money, String name) {
+        this.money = money;
+        this.name = name;
+    }
+}
+class Drawing extends Thread{
+    Account account;
+    int drawingMoney;
+    int nowMoney;
+
+    public Drawing(Account account, int drawingMoney, String name) {
+        super(name);
+        this.account = account;
+        this.drawingMoney = drawingMoney;
+    }
+
+    @Override
+    public void run() {
+
+        if (account.money-drawingMoney<0){
+            System.out.println("无钱了");
+            return;
+        }
+        account.money = account.money-drawingMoney;
+        nowMoney = nowMoney + drawingMoney;
+        System.out.println(account.name+"余额"+account.money);
+        System.out.println(this.getName()+"手里的钱"+nowMoney);
+    }
 }
